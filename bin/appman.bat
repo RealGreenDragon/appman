b859805a84ff7776b2b7e11e4dabb351b02d5c86
MagicGreenDragon <14246920+MagicGreenDragon@users.noreply.github.com>
2018-04-17 17:38:16 +0200

    Fix license data

--- bin/appman.bat
+++ bin/appman.bat
@@ -1,4 +1,18 @@
 @echo off
 
+REM -*- coding: utf-8 -*-
+
+REM Copyright (C) 2018  Daniele Giudice
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 REM Run appman.py using embed python
 %~dp0\python-embed\python -B %~dp0\..\appman.py %*
